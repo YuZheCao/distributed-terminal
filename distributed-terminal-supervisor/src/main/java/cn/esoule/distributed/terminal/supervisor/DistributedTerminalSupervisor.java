94849b76ad445480918701e92136e493c3e3bd59
caoxin1 <caoxin1@bj-m-206457a.local>
2017-07-29 23:03:05 +0800

    init

---
+++ distributed-terminal-supervisor/src/main/java/cn/esoule/distributed/terminal/supervisor/DistributedTerminalSupervisor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.esoule.distributed.terminal.supervisor;
+
+/**
+ *
+ * @author caoxin1
+ */
+public class DistributedTerminalSupervisor {
+
+    public static void main(String... args) {
+        
+    }
+}
